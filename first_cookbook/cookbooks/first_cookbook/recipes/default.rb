b089d5b241e99dab3a18925a0d7c7a955f4d5126
Mike Nolan <michael.nolan@parivedasolutions.com>
2018-10-27 17:06:22 -0500

    Updates for first cookbook

--- first_cookbook/cookbooks/first_cookbook/recipes/default.rb
+++ first_cookbook/cookbooks/first_cookbook/recipes/default.rb
@@ -3,6 +3,6 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-file "#{ENV['HOME']}/test.txt" do
-  content 'This file was created by Chef!'
+file "#{ENV['HOME']}/test-2.txt" do
+  content 'This file was created by Chef! (for mike)'
 end
